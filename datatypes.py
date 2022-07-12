994df711565176257785da1a9a7b1c3a0268f280
LionHuntDiG <nath.shanmukha@gmail.com>
2022-07-12 23:51:40 +0530

    adding practise files

---
+++ datatypes.py
@@ -0,0 +1,27 @@
+from distutils.util import copydir_run_2to3
+
+
+print( 22 / 7)
+pods= print("kubectl get po -n monitoring")
+for i in  range(1,9):
+    print(i)
+
+code = "Norwegian Blue"
+print(code[:14:3])  #Nwi u
+print(code[-15:14:-1]) 
+print(code[1:-12:2]) 
+print(code[0])
+
+new_line = print("*" * 181)
+
+
+
+
+
+
+
+
+
+
+
+
